7021af6e072d14d235d55081b0e96205a326e99e
ASH WILLIAMS <AW317330@falmouth.ac.uk>
2025-11-18 12:01:43 +0000

    0.0.1 Player Movement Working
    ++Player Movement

---
+++ The Cult of Progress/Assets/Scripts/PLAYER/PLAYER_Movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PLAYER_Movement : MonoBehaviour
+{
+    
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Called by the Input System when Move action is triggered
+    public void OnMove(InputValue inputValue)
+    {
+        moveInput = inputValue.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+}
